816f19c1cb75c21efdd51e2e483cbd138d094f21
YutoOkawa <yuutoowata.0305@gmail.com>
2024-02-15 23:54:29 +0900

    Add container component to About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,4 @@
+import { Container } from '@mui/material';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
@@ -6,20 +7,22 @@ function About() {
 		// TODO: h5 をもう少し工夫したい
 		// TODO: body1 にある要素を１つずつブロック的に区切りたい
   	<>
-			<Box p={2}>
-				<Box display={'flex'} justifyContent={'left'} p={1}>
-					<Typography variant='h5'>
-						About
-					</Typography>
-				</Box>	
-				<Box display={'flex'} justifyContent={'left'} p={1}>
-					<Typography variant='body1' align='left'>
-						Name: Yuto Okawa <br />
-						Job: Security Engineer <br />
-						Like: Golang / FIDO
-					</Typography>
+			<Container maxWidth="xl">
+				<Box p={2}>
+					<Box display={'flex'} justifyContent={'left'} p={1}>
+						<Typography variant='h5'>
+							About
+						</Typography>
+					</Box>	
+					<Box display={'flex'} justifyContent={'left'} p={1}>
+						<Typography variant='body1' align='left'>
+							Name: Yuto Okawa <br />
+							Job: Security Engineer <br />
+							Like: Golang / FIDO
+						</Typography>
+					</Box>
 				</Box>
-			</Box>
+			</Container>
     </>
   );
 }
