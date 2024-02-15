b9912607de0111ca9790004b7cfa56c81f72fccb
YutoOkawa <yuutoowata.0305@gmail.com>
2024-02-16 00:16:27 +0900

    Add Profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import NavigationBar from './components/NavigationBar'
 import About from './components/About'
+import Profile from './components/Profile'
 
 function App() {
   return (
     <>
       <NavigationBar />
+			<Profile />
       <About></About>
     </>
   );
---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import { Avatar, Box, Container, Typography } from "@mui/material";
+
+function Profile() {
+	return (
+		<>
+			<Container maxWidth='xl'>
+				<Box p={2}>
+					<Box display={'flex'} justifyContent={'left'} p={1}>
+						<Avatar 
+						  alt="my-avatar"
+							src="../static/images/IMG_2535.JPG" // TODO: 画像が見えるように
+							sx={{width:100, height:100}} // TODO: リサイズ
+						/>
+					</Box>
+					<Box display={'flex'} justifyContent={'left'} p={1}>
+						<Typography variant="h5">
+							Yuto Okawa
+						</Typography>
+					</Box>
+				</Box>
+			</Container>
+		</>
+	);
+}
+export default Profile
